2a769003da020dae7493a1d8869ce50e0edfa854
liucongzhou <1543812491@qq.com>
2023-02-26 16:54:49 +0800

    add tabbar

--- apps/chat/src/pages/_app.jsx
+++ apps/chat/src/pages/_app.jsx
@@ -1,6 +1,22 @@
 import 'react-chatbot-kit/build/main.css'
 import '../customCss/customCss.css';
+import '@nutui/nutui-react/dist/style.css'
+import { Tabbar, TabbarItem } from '@nutui/nutui-react';
+import Link from 'next/link'
+import { Icon } from '@nutui/nutui-react';
+
+
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return  <>
+        <Component {...pageProps} />
+        <Tabbar bottom onSwitch={(child, idx) => {console.log(idx)}}>
+           <TabbarItem tabTitle="登录" icon="add" />
+           <TabbarItem tabTitle="注册" icon="edit" />
+           <Link href="/chatBot">
+              <TabbarItem tabTitle="聊天" icon="dongdong"></TabbarItem>
+            </Link>
+           <TabbarItem tabTitle="我的" icon="my" />
+       </Tabbar>
+    </>
 }
--- apps/chat/src/pages/index.jsx
+++ apps/chat/src/pages/index.jsx
@@ -1,9 +1,10 @@
-import Chatbot from '../components/Chatbot';
 import { Meun } from '../components/menu/menu';
 import { io } from "socket.io-client";
 import { useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom'
 import Head from 'next/head'
+
+
 
 function guid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -71,13 +72,11 @@ function HomePage() {
            </Head>
            <div className='clear-message' style={{display: isViewTips? 'block': 'none'}}>Loose will clear message</div>
             <div className='home-container' onTouchStart={handleOnTouchStart} onTouchMove={handleOnTouchMove} onTouchEnd={handleOnTouchEnd}>
-                <Meun/>
+                {/* <Meun/> */}
                 {/* <div className='my-title-container'>
                     <div className='my-title-animate'>ChatGpt Demo</div>
                     <div className='my-title'>ChatGpt Demo</div>
                 </div> */}
-                
-                <Chatbot />
             </div>
             <div className='my-tips'>
                 这是一个chatGPT的demo，由于经费有限，连续对话太多或刷新网页可能会导致她忘了您哦~
