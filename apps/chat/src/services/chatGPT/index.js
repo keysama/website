5fc18ae70219b6cb295e94904b0b00c6456b75dc
key <834529965@qq.com>
2023-02-28 23:23:41 +0800

    formate

--- apps/chat/src/services/chatGPT/index.js
+++ apps/chat/src/services/chatGPT/index.js
@@ -2,95 +2,97 @@ import axios from 'axios';
 export const START_SIGN = '🔨';
 export const END_SIGN = '🪐';
 
-function defaultHandle(value){
+function defaultHandle(value) {
     console.log("value:", value)
 }
 
-class StreamResolver{
-    constructor(callback){
+class StreamResolver {
+    constructor(callback) {
         this.mode = 'before';
         this.before = '';
         this.after = '';
         this.callback = callback;
     }
 
-    changeMode(mode){
+    changeMode(mode) {
         this.mode = mode;
     }
 
-    appendText(text){
+    appendText(text) {
         this[this.mode] += text;
-        this.callback({type: this.mode, text: text})
+        this.callback({ type: this.mode, text: text })
     }
 
-    input(text = ''){
+    input(text = '') {
         let newText = text;
-        if(this[this.mode]){
+        if (this[this.mode]) {
             newText = text.replace(this[this.mode], '');
         }
 
         this.splitSign(newText);
     }
 
-    splitSign(text){
-        if(text.startsWith(START_SIGN)){
-            const [,b] = text.split(START_SIGN);
+    splitSign(text) {
+        if (text.startsWith(START_SIGN)) {
+            const [, b] = text.split(START_SIGN);
             this.input(b);
             return;
-        }else if(text.includes(END_SIGN) && this.mode !== 'after'){
-            const [a,b] = text.split(END_SIGN);
+        } else if (text.includes(END_SIGN) && this.mode !== 'after') {
+            const [a, b] = text.split(END_SIGN);
             this.appendText(a);
             this.mode = 'after';
             this.appendText(b);
             return;
-        }else{
+        } else {
             this.appendText(text);
-        } 
+        }
     }
 }
 
-export async function sendMessage(message, handle = defaultHandle){
+export async function sendMessage(message, handle = defaultHandle) {
     const sessionId = localStorage.getItem('sessionId');
     const messageId = localStorage.getItem('messageId');
     const conversationId = localStorage.getItem('conversationId');
     const uid = localStorage.getItem('uid');
-    let offset = 0;
     let targetMessage = '';
     let reciveString = '';
-    const streamResolver = new StreamResolver(({type, text}) => {
-        if(type === 'before'){
+    const streamResolver = new StreamResolver(({ type, text }) => {
+        if (type === 'before') {
             targetMessage += text;
             handle(text)
-        }else{
+        } else {
             reciveString += text;
         }
     });
 
-    function onDownloadProgress(progressEvent){
+    function onDownloadProgress(progressEvent) {
         const { event } = progressEvent;
         const { currentTarget } = event;
-        const chunk = currentTarget.responseText.substring(offset);
+        const chunk = currentTarget.responseText;
         streamResolver.input(chunk);
     }
 
-    await axios.post('/api/chat', {
-        text: message,
-        sessionId: sessionId,
-        messageId: messageId,
-        conversationId:conversationId,
-        uid: uid
-    },{ timeout: 5 * 60 * 1000, onDownloadProgress });
+    let res = null;
 
-    let res = '';
-    try{
+    try {
+        await axios.post('/api/chat', {
+            text: message,
+            sessionId: sessionId,
+            messageId: messageId,
+            conversationId: conversationId,
+            uid: uid
+        }, { timeout: 5 * 60 * 1000, onDownloadProgress });
+
         res = JSON.parse(reciveString);
-    }catch(e){
+    } catch (e) {
         console.log('err:', e)
     }
 
-    localStorage.setItem('sessionId', res.sessionId)
-    localStorage.setItem('messageId', res.messageId)
-    localStorage.setItem('conversationId', res.conversationId)
+    if (res) {
+        localStorage.setItem('sessionId', res.sessionId)
+        localStorage.setItem('messageId', res.messageId)
+        localStorage.setItem('conversationId', res.conversationId)
+    }
 
     return targetMessage;
 }
