1036809023a27ebf8602fa0ca07acd634f8e0134
Leo31 <leo31@michaels.com>
2023-03-04 17:58:50 +0800

    add css

--- apps/chat/src/pages/_app.jsx
+++ apps/chat/src/pages/_app.jsx
@@ -10,10 +10,10 @@ import { Icon } from '@nutui/nutui-react';
 export default function MyApp({ Component, pageProps }) {
     return  <>
         <Component {...pageProps} />
-        <Tabbar bottom onSwitch={(child, idx) => {console.log(idx)}}>
+        <Tabbar bottom onSwitch={(child, idx) => {console.log(idx)}} className="tabbar">
            <TabbarItem tabTitle="登录" icon="add" />
            <TabbarItem tabTitle="注册" icon="edit" />
-           <Link href="/chatBot">
+           <Link href="/chatBot" className='nut-tabbar-item'>
               <TabbarItem tabTitle="聊天" icon="dongdong"></TabbarItem>
             </Link>
            <TabbarItem tabTitle="我的" icon="my" />
