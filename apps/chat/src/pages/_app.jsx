fce9a7402f5519e72c2fea880b3e7ab3ad064aa4
Leo31 <leo31@michaels.com>
2023-03-04 18:30:11 +0800

    change tab csss

--- apps/chat/src/pages/_app.jsx
+++ apps/chat/src/pages/_app.jsx
@@ -4,18 +4,25 @@ import '@nutui/nutui-react/dist/style.css'
 import { Tabbar, TabbarItem } from '@nutui/nutui-react';
 import Link from 'next/link'
 import { Icon } from '@nutui/nutui-react';
+import * as React from 'react'
+import { useRouter } from 'next/router'
 
 
 
 export default function MyApp({ Component, pageProps }) {
+    const [activeIndex, setActiveIndex] = React.useState(0)
+    const router = useRouter()
+
     return  <>
         <Component {...pageProps} />
-        <Tabbar bottom onSwitch={(child, idx) => {console.log(idx)}} className="tabbar">
+        <Tabbar activeVisible={activeIndex} bottom   onSwitch={(child, id) => {
+               if(id === 2){
+                router.push('/chatBot')
+               }
+               setActiveIndex(id)}}  className="tabbar">
            <TabbarItem tabTitle="登录" icon="add" />
            <TabbarItem tabTitle="注册" icon="edit" />
-           <Link href="/chatBot" className='nut-tabbar-item'>
-              <TabbarItem tabTitle="聊天" icon="dongdong"></TabbarItem>
-            </Link>
+           <TabbarItem tabTitle="聊天" icon="dongdong"></TabbarItem>
            <TabbarItem tabTitle="我的" icon="my" />
        </Tabbar>
     </>
