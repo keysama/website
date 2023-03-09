0fcca2d063997867c9cbcdae89f3a3025f68adf0
key <834529965@qq.com>
2023-03-10 01:08:39 +0800

    optimize eslint&typescript

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,4 +4,19 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'prettier',
     ],
+    env: {
+        node: true,
+        jest: true,
+    },
+    rules: {
+        'import/prefer-default-export': 'off',
+        'import/extensions': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
 };
--- servers/api/.eslintrc.js
+++ servers/api/.eslintrc.js
@@ -1,20 +1,5 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: 'tsconfig.json',
-        tsconfigRootDir: __dirname,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-    env: {
-        node: true,
-        jest: true,
-    },
-    ignorePatterns: ['.eslintrc.js'],
+    // ignorePatterns: ['.eslintrc.js'],
     rules: {
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
